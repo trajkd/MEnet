3ff5b43d4a9efc4c2dfe590974cf350afbc39c98
trajkd <trajkd@gmail.com>
2020-06-24 21:40:49 +0200

    fix: from __future__ import urllib

--- main-aws.py
+++ main-aws.py
@@ -9,7 +9,8 @@ os.environ['AWS_PROFILE'] = "Default"
 os.environ['AWS_DEFAULT_REGION'] = "us-west-2"
 
 from datetime import date
-import urlparse
+
+from __future__ import urllib
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -273,7 +274,7 @@ class NewPostHandler(webapp2.RequestHandler):
                                         'quod': date.today().strftime("%B %d, %Y"),
                                         'content': self.request.get('content'),
                                         'cover': self.request.get('cover'),
-                                        'permalink': urlparse.quote_plus(self.request.get('title').lower())
+                                        'permalink': urllib.parse.quote_plus(self.request.get('title').lower())
                                     }
                                 )
                                 all_posts(True)
@@ -311,7 +312,7 @@ class EditBookHandler(webapp2.RequestHandler):
                         if u != None and u[0]['password'].split(",")[0]==password_hash:
                                 #b = Book.get_by_id(long(book_id))
                                 table = get_post_table()
-                                title = urlparse.unquote_plus(post_id)
+                                title = urllib.parse.unquote_plus(post_id)
                                 if self.request.get('title') != '':
                                         response = table.update_item(
                                             Key={
@@ -436,7 +437,7 @@ class Handler(webapp2.RequestHandler):
 
 class PermalinkHandler(Handler):
         def get(self, post_id):
-                p = single_post(urlparse.unquote_plus(post_id))
+                p = single_post(urllib.parse.unquote_plus(post_id))
                 if not p:
                         self.error(404)
                         return
