1f81b6ca84db04b812a47d2e30d8d99766529468
trajkd <trajkd@gmail.com>
2020-06-30 18:56:35 +0200

    fix: update Content-Type header

--- main-aws.py
+++ main-aws.py
@@ -235,6 +235,7 @@ class MailPHPPage(webapp2.RequestHandler):
             mydata = urllib.urlencode(mydata)
             path = 'http://www.mindempathy.net/mail.php'    #the url you want to POST to
             req = urllib2.Request(path, mydata)
+            req.add_header("Content-type", "application/x-www-form-urlencoded")
             try:
                 urllib2.urlopen(req)
             except HTTPError as e:
