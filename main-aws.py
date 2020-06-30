3345d2cbe123cfe82d0dae093abe99b0e2e55e59
trajkd <trajkd@gmail.com>
2020-06-30 18:49:07 +0200

    fix: use urllib2 instead of urllib

--- main-aws.py
+++ main-aws.py
@@ -235,7 +235,7 @@ class MailPHPPage(webapp2.RequestHandler):
             path = 'http://www.mindempathy.net/mail.php'    #the url you want to POST to
             req = urllib2.Request(path, mydata)
             req.add_header("Content-Type", "multipart/form-data")
-            handler = urllib.request.urlopen(req)
+            urllib2.urlopen(req)
 
 def query_authors(username, dynamodb=None):
     if not dynamodb:
