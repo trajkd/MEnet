93f508b2e7abf664d18a56ae664908f98bf5c3fd
trajkd <trajkd@gmail.com>
2020-06-30 16:51:44 +0200

    fix: Link overlay + insecure mail

--- mail.php
+++ mail.php
@@ -143,6 +143,13 @@ $html .='
 $mail = new PHPMailer(true);
 
 try {
+	$mail->SMTPOptions = array(
+		'ssl' => array(
+			'verify_peer' => false,
+			'verify_peer_name' => false,
+			'allow_self_signed' => true
+		)
+	);
 	//Server settings
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = CONFIG['email']['host'];                    // Set the SMTP server to send through
