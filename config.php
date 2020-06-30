37db419566d828bbc8f0699336c80ae21fa8d10f
trajkd <trajkd@gmail.com>
2020-06-30 16:27:06 +0200

    feat: Add contact functionality

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+const CONFIG = [
+	'email' => [
+		'host' => 'smtp.zoho.eu',
+		'port' => 587,
+		'username' => 'moreondt@gmail.com',
+		'password' => 'Dragana*7',
+		'SMTPSecure' => 'tls'
+	]
+];
